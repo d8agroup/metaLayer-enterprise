a24c7c33e1604297bb324e9219e2c79b4a587012
Matthew Griffiths <mg@metalayer.com>
2012-03-16 09:41:42 +0300

    updated submodule Added post aggregator urls to the main url file

--- urls.py
+++ urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     #Special rules to overload the normal dashboard functions
     url(r'dashboard/widgets/get_all', load_project_widgets),
 
+    url(r'^aggregation/', include('metalayercore.aggregator.urls')),
     url(r'^admin/', include('enterprise.admin.urls')),
     url(r'^dashboard/', include('enterprise.thedashboard.urls')),
     url(r'^i/', include('enterprise.imaging.urls')),
