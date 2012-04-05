59953b6d155253aae22c4dee8a40293c16e5b1da
Matthew Griffiths <mg@metalayer.com>
2012-04-05 14:17:50 +0300

    updated .gitignore added uplaod test.py added fabfile

---
+++ assets/performance/uploader.py
@@ -0,0 +1,99 @@
+########################################################################################################################
+# TEST VARIABLE SETTINGS
+########################################################################################################################
+total_number_of_items_to_include_in_test = 5000
+total_number_of_threads_to_use = 2
+number_of_content_items_to_include_in_each_post = 500
+post_url = 'http://localhost/aggregation/post_content'
+skip_duplicate_check = 'true'
+actions_to_apply_to_each_content_item = [
+    {'name':'localsentimentanalysis'},
+    {'name':'localtagging'},
+]
+########################################################################################################################
+# END TEST VARIABLE SETTINGS
+########################################################################################################################
+
+
+
+from random import randint
+import sys
+import urllib, urllib2
+import json
+import datetime
+from Queue import Queue
+import threading
+
+
+finished = []
+
+class Poster(threading.Thread):
+    def __init__(self, content, thread_count):
+        self.content = content
+        self.thread_count = thread_count
+        self.time_taken = 0
+        threading.Thread.__init__(self)
+
+    def run(self):
+        try:
+            start_time = datetime.datetime.now()
+            post_data = {
+                'content':json.dumps(self.content),
+                'actions':json.dumps(actions_to_apply_to_each_content_item),
+                'skip_duplicate_check':skip_duplicate_check,
+                }
+            response = urllib2.urlopen(post_url, urllib.urlencode(post_data)).read()
+            time_taken = datetime.datetime.now() - start_time
+            self.time_taken = time_taken
+            print 'thread: %i - finished in %s  (%s)' % (self.thread_count, time_taken, response)
+        except Exception as e:
+            print 'thread: %i - error %s' % (self.thread_count, e)
+            self.time_taken = None
+
+def producer(q, content_packages):
+    x = 1
+    for package in content_packages:
+        thread = Poster(package, x)
+        thread.start()
+        q.put(thread, True)
+        x += 1
+
+def consumer(q, package_count):
+    while len(finished) < package_count:
+        thread = q.get(True)
+        thread.join()
+        finished.append(thread.thread_count)
+
+stock_tweets = [
+        {'source': {'display_name': 'TestData', 'id': '932ef63c661d734ba7f2d11b88b815c0'}, 'link': 'http://twitter.com/LovelyDeadBear/statuses/181972701051752449', 'time': 1332209847, 'text': [{'title': u'#DoritosLocosTacos one of the best parts of my spring break - candymountain'}], 'author': {'link': 'http://twitter.com/lovelydeadbear', 'image': 'http://purl.org/net/spiurl/lovelydeadbear', 'display_name': 'lovelydeadbear'}, 'id': 'dbb3a269c5c50548ab32024ef69e0f38', 'channel': {'type': 'testdata', 'id': '85d52cc25a3e0c30262de5426b94ea09', 'sub_type': 'testdata'}}
+]
+data = []
+for x in range(total_number_of_items_to_include_in_test):
+    i = stock_tweets[randint(0, len(stock_tweets) - 1)].copy()
+    i['id'] = '%i' % x
+    data.append(i)
+
+content_packages = []
+number_of_items_sent = 0
+for r in range(0, len(data), number_of_content_items_to_include_in_each_post):
+    if r >= total_number_of_items_to_include_in_test:
+        break
+    post_data = data[r:r + number_of_content_items_to_include_in_each_post]
+    content_packages.append(post_data)
+    number_of_items_sent += len(post_data)
+
+start_time = datetime.datetime.now()
+queue = Queue(total_number_of_threads_to_use)
+producer_thread = threading.Thread(target=producer, args=(queue, content_packages))
+consumer_thread = threading.Thread(target=consumer, args=(queue, len(content_packages)))
+producer_thread.start()
+consumer_thread.start()
+producer_thread.join()
+consumer_thread.join()
+print '******************************************************'
+print 'All threads finished in %s' % (datetime.datetime.now() - start_time)
+print 'Average time to one item = %s' % ((datetime.datetime.now() - start_time) / number_of_items_sent)
+print 'Average object size = %f bytes' % (float(sum([sys.getsizeof(i) for i in data])) / len(data))
+print 'Queue size: %i' % total_number_of_threads_to_use
+print 'Post size: %i' % number_of_content_items_to_include_in_each_post
+print '******************************************************'
---
+++ fabfile.py
@@ -0,0 +1,203 @@
+from __future__ import with_statement
+import os
+from fabric.api import *
+
+packages = {
+    'pip':[
+        'python-dateutil==1.5',
+        'feedparser==5.1',
+        'raven==1.4.6',
+        'pyyaml',
+        '-U distribute',
+        'nltk'
+    ],
+    'apt-get':[
+        'python-memcache',
+    ]
+}
+
+def dev():
+    env.hosts = ['root@me.dev']
+
+def stage():
+    pass
+
+def clienttest():
+    env.hosts = ['root@me.clienttest.web.01']
+
+def prod():
+    pass
+
+def git():
+    for dir in ['metalayercore', 'thedashboard', '.']: #'compressor', 'chargifyapi',
+        with settings(warn_only=True):
+            local('cd /home/matt/code/metaLayer/enterprise/%s && git add --all && git commit && git push' % dir)
+
+def fetchall(branch='master'):
+    for dir in ['metalayercore', 'thedashboard',  '.']: #'compressor=develop', 'chargifyapi',
+        with settings(warn_only=True):
+            this_branch = dir.split('=')[1] if '=' in dir else branch
+            local('cd /home/matt/code/metaLayer/enterprise/%s && git fetch && git merge origin/%s' % (dir.split('=')[0], this_branch))
+
+def deploy(install_packages=False):
+    if install_packages:
+        for package in packages['pip']:
+            run('pip install %s' % package)
+        for package in packages['apt-get']:
+            run('apt-get install %s' % package)
+
+    with cd('/usr/local/metaLayer-enterprise/enterprise'):
+        run("git pull")
+        run("git submodule init && git submodule update")
+        run("git status")
+    with settings(warn_only=True):
+        run("service apache2 restart")
+        run("service nginx restart")
+
+def recycle():
+    run('service apache2 restart')
+
+def runcommand():
+    #methd used for ad hoc commands that need to be run
+    #run('pip uninstall django_compressor')
+    #run('mkdir /var/log/metalayer')
+    #run('touch /var/log/metalayer/errors.log')
+    #run('chmod a+rw /var/log/metalayer/errors.log')
+    #run('ln -s /usr/local/metaLayer-dashboard/dashboard/imaging/CACHE /usr/local/metaLayer-dashboard/dashboard/static/CACHE/images')
+    #run('rm /usr/local/metaLayer-dashboard/dashboard/static/CACHE/images')
+    """
+    with cd('/usr/local/metaLayer-dashboard/dashboard'):
+        with settings(warn_only=True):
+            run('rm actions -r')
+            run('rm aggregator -r')
+            run('rm dashboards -r')
+            run('rm datapoints -r')
+            run('rm outputs -r')
+            run('rm visualizations -r')
+            run('rm search -r')
+    """
+    #run('ln -s /usr/local/metaLayer-enterprise/enterprise/thedashboard/static/css /usr/local/metaLayer-enterprise/enterprise/static/css/thedashboard')
+    #run('ln -s /usr/local/metaLayer-enterprise/enterprise/thedashboard/static/images /usr/local/metaLayer-enterprise/enterprise/static/images/thedashboard')
+    #run('ln -s /usr/local/metaLayer-enterprise/enterprise/thedashboard/static/html /usr/local/metaLayer-enterprise/enterprise/static/html/thedashboard')
+    #run('ln -s /usr/local/metaLayer-enterprise/enterprise/thedashboard/static/js /usr/local/metaLayer-enterprise/enterprise/static/js/thedashboard')
+    #run('mkdir /usr/local/metaLayer-enterprise/enterprise/static/CACHE')
+    #run('ln -s /usr/local/metaLayer-enterprise/enterprise/imaging/CACHE /usr/local/metaLayer-enterprise/enterprise/static/CACHE/images')
+    #run('chmod a+rw /usr/local/metaLayer-enterprise/enterprise/static/CACHE')
+    #run('chmod a+rw /usr/local/metaLayer-enterprise/enterprise/imaging/CACHE')
+    pass
+
+def serversetup(server_ip=None, git_managed=False):
+    apachesetup()
+    codebasesetup(server_ip, git_managed)
+    solrsetup()
+
+
+def apachesetup():
+    ####################################################################################################################
+    # APACHE2 SETUP
+    ####################################################################################################################
+    run('apt-get update')
+    run('apt-get install apache2 libapache2-mod-wsgi')
+    run('rm /etc/apache2/sites-enabled/*')
+    run('a2enmod headers')
+    run('service apache2 restart')
+    put('%s/assets/apache2/vhost' % os.path.dirname(__file__), '/etc/apache2/sites-enabled/metaLayer-enterprise', mode=0755)
+
+def codebasesetup(server_ip=None, git_managed=False):
+    ####################################################################################################################
+    # CODEBASE SETUP
+    ####################################################################################################################
+    run('apt-get update')
+    run('apt-get install git-core mercurial mongodb python-cairo python-rsvg python-pip')
+    run('pip install --upgrade pip')
+    for package in packages['pip']:
+        run('pip install %s' % package)
+    for package in packages['apt-get']:
+        run('apt-get install %s' % package)
+    with settings(warn_only=True):
+        system_packages = run('pip freeze -l').splitlines()
+        system_packages = [p.split('==')[0] for p in system_packages]
+        if 'Django' not in system_packages:
+            run('pip install hg+https://bitbucket.org/wkornewald/django-nonrel')
+        if 'djangotoolbox' not in system_packages:
+            run('pip install hg+https://bitbucket.org/wkornewald/djangotoolbox')
+        if 'django-mongodb-engine' not in system_packages:
+            run('pip install git+git://github.com/django-nonrel/mongodb-engine')
+    with settings(warn_only=True):
+        run('ssh-keygen -t rsa')
+    run('more ~/.ssh/id_rsa.pub')
+    run('read -p "Press ENTER once ssh key is installed"')
+    with settings(warn_only=True):
+        run('rm /tmp/metaLayer-enterprise -r')
+        run('rm /usr/local/metaLayer-enterprise -r')
+        run('mkdir /tmp/metaLayer-enterprise')
+        run('mkdir /usr/local/metaLayer-enterprise')
+        run('mkdir /usr/local/metaLayer-enterprise/enterprise')
+    put('%s/assets/apache2/wsgi' % os.path.dirname(__file__), '/usr/local/metaLayer-enterprise/enterprise.wsgi', mode=0755)
+    put('%s/assets/users/create_users' % os.path.dirname(__file__), '/tmp/create_users', mode=0755)
+
+    with settings(warn_only=True):
+        run('rm /usr/share/nltk_data -r')
+    run('mkdir /usr/share/nltk_data')
+    run("python -c \"import nltk;nltk.download('brown', download_dir='/usr/share/nltk_data')\"")
+
+    if git_managed:
+        run('git clone git@github.com:metaLayer/metaLayer-enterprise.git /usr/local/metaLayer-enterprise/enterprise')
+        with cd('/usr/local/metaLayer-enterprise/enterprise'):
+            run('git checkout master')
+            run('git submodule init && git submodule update')
+        run('cd /usr/local/metaLayer-enterprise/enterprise && python manage.py syncdb')
+        with cd('cd /usr/local/metaLayer-enterprise/enterprise'):
+            run('more /tmp/create_users | python manage.py shell')
+        run('rm /tmp/create_users')
+    else:
+        run('git clone git@github.com:metaLayer/metaLayer-enterprise.git /tmp/metaLayer-enterprise')
+        with cd('/tmp/metaLayer-enterprise'):
+            run('git checkout master')
+            run('git submodule init && git submodule update')
+        run('cp /tmp/metaLayer-enterprise/* /usr/local/metaLayer-enterprise/enterprise/ -r')
+        run('sed -i \'s/ENTER_SITE_HOST/%s/g\' /usr/local/metaLayer-enterprise/enterprise/settings.py' % server_ip)
+        run('cd /usr/local/metaLayer-enterprise/enterprise && python manage.py syncdb')
+        run('python -c "import compileall; compileall.compile_dir(\'/usr/local/metaLayer-enterprise/enterprise\', force=1);"')
+        run('find /usr/local/metaLayer-enterprise/enterprise -name "*.py" -delete')
+        with cd('/usr/local/metaLayer-enterprise/enterprise'):
+            run('more /tmp/create_users | python manage.pyc shell')
+        run('rm /tmp/create_users')
+
+    run('rm /tmp/metaLayer-enterprise -r')
+    run('ln -s /usr/local/metaLayer-enterprise/enterprise/thedashboard/static/css /usr/local/metaLayer-enterprise/enterprise/static/css/thedashboard')
+    run('ln -s /usr/local/metaLayer-enterprise/enterprise/thedashboard/static/images /usr/local/metaLayer-enterprise/enterprise/static/images/thedashboard')
+    run('ln -s /usr/local/metaLayer-enterprise/enterprise/thedashboard/static/html /usr/local/metaLayer-enterprise/enterprise/static/html/thedashboard')
+    run('ln -s /usr/local/metaLayer-enterprise/enterprise/thedashboard/static/js /usr/local/metaLayer-enterprise/enterprise/static/js/thedashboard')
+    run('mkdir /usr/local/metaLayer-enterprise/enterprise/static/CACHE')
+    run('ln -s /usr/local/metaLayer-enterprise/enterprise/imaging/CACHE /usr/local/metaLayer-enterprise/enterprise/static/CACHE/images')
+    run('chmod a+rw /usr/local/metaLayer-enterprise/enterprise/static/CACHE')
+    run('chmod a+rw /usr/local/metaLayer-enterprise/enterprise/imaging/CACHE')
+    run('service apache2 restart')
+    run('curl http://localhost')
+
+def solrsetup():
+    ####################################################################################################################
+    # SOLR SETUP
+    ####################################################################################################################
+    run('apt-get update')
+    run('apt-get install tomcat6')
+    with settings(warn_only=True):
+        run('rm /var/solr -r')
+        run('rm /tmp/solr -r')
+    run('mkdir /tmp/solr')
+    run('mkdir /var/solr')
+    with cd('/tmp/solr'):
+        run('wget http://mirror.cc.columbia.edu/pub/software/apache/lucene/solr/3.4.0/apache-solr-3.4.0.tgz')
+        run('tar xzf apache-solr-3.4.0.tgz')
+        run('cp apache-solr-3.4.0/dist/apache-solr-3.4.0.war /var/solr/solr.war')
+        run('cp apache-solr-3.4.0/example/solr/* /var/solr -r')
+    run('rm /var/solr/conf/schema.xml')
+    put('%s/assets/solr/schema.xml' % os.path.dirname(__file__), '/var/solr/conf/schema.xml', mode=0755)
+    run('chown -R tomcat6 /var/solr/')
+    run("echo -e '<Context docBase=\"/var/solr/solr.war\" debug=\"0\" privileged=\"true\" allowLinking=\"true\" crossContext=\"true\">\n<Environment name=\"solr/home\" type=\"java.lang.String\" value=\"/var/solr\" override=\"true\" />\n</Context>' | tee -a /etc/tomcat6/Catalina/localhost/solr.xml")
+    run("echo 'TOMCAT6_SECURITY=no' | sudo tee -a /etc/default/tomcat6")
+    run('sed -i \'s/set -e/set -e\\nJAVA_OPTS="$JAVA_OPTS -Dsolr.home=\/var\/solr"/g\' /etc/init.d/tomcat6')
+    run('service tomcat6 restart')
+    run('curl http://localhost:8080/solr/select/?q=*')
+    run('read -p "PLEASE CHECK THE SOLR OUTPUT ABOVE"')
