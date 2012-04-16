10c3aee88e2e8bd51d2613ce2a067bac061f1dd4
Matthew Griffiths <mg@metalayer.com>
2012-04-16 14:57:26 -0400

    added outputs back into the top level urls file

--- urls.py
+++ urls.py
@@ -10,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include('enterprise.admin.urls')),
     url(r'^dashboard/', include('enterprise.thedashboard.urls')),
     url(r'^i/', include('enterprise.imaging.urls')),
+    url(r'^o/', include('metalayercore.outputs.urls')),
     url(r'', include('enterprise.themanager.urls')),
 )
