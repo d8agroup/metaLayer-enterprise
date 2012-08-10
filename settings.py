c1e6a3e8a301d6ef2027ecec387e896b1783e490
Rich Schumacher <rich.schu@gmail.com>
2012-08-10 16:46:50 +0000

    Update settings.py to dynamically create filesystem paths.
    Rather than hard-coding the directories for assets determine the
    directories relative to the settings.py file.

--- settings.py
+++ settings.py
@@ -1,3 +1,5 @@
+import os
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -33,13 +35,15 @@ IMAGE_HOST = SITE_HOST
 
 DYNAMIC_IMAGES_WEB_ROOT = '/static/CACHE/images/'
 
-DYNAMIC_IMAGES_ROOT = '/usr/local/metaLayer-enterprise/enterprise/metalayercore/imaging/CACHE/'
+BASE_ROOT = os.path.dirname(os.path.abspath(__file__))
 
-THEMES_ROOT = '/usr/local/metaLayer-enterprise/enterprise/static/themes'
+DYNAMIC_IMAGES_ROOT = BASE_ROOT + '/metalayercore/imaging/CACHE/'
+
+THEMES_ROOT = BASE_ROOT + '/static/themes'
 
 SENTRY_DSN = 'http://cb24eedee3b149c0966cb312dedcbd8c:598474230fe84813bfd9d0da84098d2e@108.166.111.61:9000/5'
 
-TEMPLATE_DIRS = ( '/usr/local/metaLayer-enterprise/enterprise/static/html', )
+TEMPLATE_DIRS = ( BASE_ROOT + '/static/html', )
 
 SITE_URLS = {
     'company_prefix':'company'
