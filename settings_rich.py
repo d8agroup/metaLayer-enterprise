d8581342a2ead8fb662369642660ed09411a6a04
Matthew Griffiths <mg@metalayer.com>
2012-09-24 20:16:05 +0100

    36578571-Code_changes_for_solr4
     * Updated submodules
     * Updated config for different environments to support solr4

--- settings_rich.py
+++ settings_rich.py
@@ -6,7 +6,8 @@ SOLR_CONFIG = {
     'default_page_size':100,
     'solr_url':'http://108.166.59.173:8080/solr',
     'solr_params':SOLR_PARAMS,
-    'solr_facets':{}
+    'solr_facets':{},
+    'solr_update_url':'update/json'
 }
 
 EMAIL_USE_TLS = True
