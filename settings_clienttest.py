62ae133211d0f663bc8e8747f37acb10c272396a
Matthew Griffiths <mg@metalayer.com>
2012-09-25 12:22:00 +0100

    ADHOC - Added Facebook Oauth settings to demo config

--- settings_clienttest.py
+++ settings_clienttest.py
@@ -40,6 +40,11 @@ OAUTH2_SETTINGS = {
         'client_id':'450032264506-i36s4e0mqkjnhmquq0bv2617vh44nu12.apps.googleusercontent.com',
         'client_secret':'8fDtqnPEmhTeUD4th_lLd1RT',
         'redirect_uri':'http://%s/oauth2/google_oauth2_callback' % SITE_HOST
+    },
+    'FacebookOauth2Controller': {
+        'client_id': '457768257589473',
+        'client_secret': 'd93ba13ce81f2f578f345865aaea2269',
+        'redirect_uri':'http://%s/oauth2/facebook_oauth2_callback' % SITE_HOST
     }
 }
 
